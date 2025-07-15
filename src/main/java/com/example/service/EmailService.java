8c5c87a93036afffff5401afc06c9e9da710257a
nicto <valli.kosmopolitov@gmail.com>
2025-07-15 16:38:30 +0300

    Spring Зад.1.4

--- src/main/java/com/example/service/EmailService.java
+++ src/main/java/com/example/service/EmailService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-
     private final JdbcTemplate jdbcTemplate ;
 
     public EmailService(JdbcTemplate jdbcTemplate) {
