88ee93c0cfc7c7256cb6bdc7b2f3da5644e07def
nicto <valli.kosmopolitov@gmail.com>
2025-07-18 08:40:20 +0300

    Spring Зад.1.5.1

--- src/main/java/com/example/service/BookService.java
+++ src/main/java/com/example/service/BookService.java
@@ -1,31 +1,50 @@
 package com.example.service;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+
 
 @Service
-public class CustomersService {
+public class BookService {
+    
 
     private final JdbcTemplate jdbcTemplate;
 
-    public CustomersService(JdbcTemplate jdbcTemplate) {
+    public BookService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void findAll() {
-        String sql = "SELECT * FROM customers";
-        jdbcTemplate.query(sql, (rs) -> {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String email = rs.getString("email");
-            System.out.println("ID: " + id + ", Name: " + name + ", Email: " + email);
+    public void reserveBook(Integer bookId, Integer readerId) {
+        String sql = "INSERT INTO reservation (book_id, reader_id, get_date, promise_date) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, (rs) -> {
+            rs.setInt(1, bookId);
+            rs.setInt(2, readerId);
+            rs.setDate(3, new Date(System.currentTimeMillis()));
+            rs.setDate(4, new Date(System.currentTimeMillis() + 15L * 24 * 60 * 60 * 1000));
+        });
+    }
+
+    public void returnBook(Integer  bookId, Integer readerId) {
+        String sql = "UPDATE reservation SET return_date = ? " + "WHERE reader_id = ? and book_id = ?";
+        jdbcTemplate.update(sql,(rs) -> {
+            rs.setDate(1, new Date(System.currentTimeMillis()));
+            rs.setInt(2, readerId);
+            rs.setInt(3,bookId);
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
