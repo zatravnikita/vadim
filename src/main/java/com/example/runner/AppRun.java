b68ffb57b3d3773c99dd3ac2e21dd579857aaed3
nicto <valli.kosmopolitov@gmail.com>
2025-07-21 16:33:49 +0300

    Spring Зад.1.5.2

--- src/main/java/com/example/runner/AppRun.java
+++ src/main/java/com/example/runner/AppRun.java
@@ -5,8 +5,8 @@ import com.example.service.BookService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.sql.Date;
-import java.util.Random;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 
@@ -22,27 +22,63 @@ public class AppRun implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+
         int x;
-        System.out.print("1 - Вернуть книгу ");
-        System.out.println("2 - Взять книгу");
+
+        System.out.print("1 - Вять книгу ");
+        System.out.println("2 - Вернуть книгу");
         System.out.println("3 - Выход");
 
         do {
             x = scanner.nextInt();
-                if(x == 1 || x == 2) {
-                System.out.println("Введи id книги");
-                int bookId = scanner.nextInt();
+            if (x == 1 || x == 2) {
 
-                System.out.println("Введи id читателя");
-                int readerId = scanner.nextInt();
+
 
                 switch (x) {
                     case 1:
-                        bookService.reserveBook(bookId, readerId);
-                        System.out.println("Добавление новой записи прошло успешно");
+
+                        System.out.println("Введите Id читателя");
+                        int readerId = scanner.nextInt();
+
+                        scanner.nextLine();
+
+                        System.out.println("Введите автора книги");
+                        String authorBook = scanner.nextLine();
+
+                        System.out.println("Введите название книги");
+                        String workBook = scanner.nextLine();
+
+                        System.out.println("Введите номер копии книги");
+                        int copyBook = scanner.nextInt();
+
+
+                        System.out.println("Введите дату обещанного возврата (в формате ГГГГ-ММ-ДД):");
+                        String stringPromise = scanner.next();
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                        LocalDate promiseDate = LocalDate.parse(stringPromise, formatter);
+
+                        bookService.reserveBook(authorBook, workBook, copyBook, promiseDate, readerId);
+                        System.out.println("Обновление данных прошло успешно");
                         break;
+
                     case 2:
-                        bookService.returnBook(bookId, readerId);
+
+                        System.out.println("Введите автора книги");
+                        String authorBookReturn = scanner.next();
+                        scanner.nextLine();
+
+                        System.out.println("Введите название книги");
+                        String workBookReturn = scanner.next();
+
+                        System.out.println("Введите Id читателя");
+                        int readerIdReturn = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.println("Номер копии книги");
+                        int copyBookReturn = scanner.nextInt();
+
+                        bookService.returnBook(readerIdReturn, authorBookReturn, workBookReturn,  copyBookReturn);
                         System.out.println("Обновление данных прошло успешно");
                         break;
                     default:
